1aa3dab696bc872fb6caacdfeef2c70112c1bb65
Manuel Dudda <manuel@pixoona.de>
2014-06-19 18:54:05 +0200

    Übungsblatt 06, Aufgabe 1 Spec-Umgebung integriert

--- lib/blatt06.rb
+++ lib/blatt06.rb
@@ -2,17 +2,69 @@ class Blatt06
   RDF_FILE = (Rails.root.join 'lib', 'assets', 'himmelskoerper.rdf').to_s
   QUERYABLE = RDF::Repository.load(RDF_FILE)
 
+  # Objekte, die um die Sonne oder um einen Satelliten der Sonne kreisen.
+  # Merkur, Venus, Erde und Mars umkreisen die Sonne
+  # Mond umkreist die Erde, Phobos und Deimos den Mars
 
-  def self.aufgabe1
+  def self.aufgabe1a
     str = '
       PREFIX ex: <http://example.org/>
-      SELECT ?himmelskoerper WHERE
+      SELECT ?object ?satellite WHERE
       {
-        ex:Sonne ex:satellit ?himmelskoerper .
+       ?object ex:satellit ?satellite .
       }'
     sparql = SPARQL.parse(str)
     solutions = QUERYABLE.query(sparql)
-    solutions.each { |sol| puts sol.himmelskoerper }
+  end
+
+  def self.aufgabe1b
+    str = "
+      PREFIX ex: <http://example.org/>
+      SELECT DISTINCT ?object ?parent ?radius WHERE
+      {
+        { ?object ex:radius ?radius . }
+        OPTIONAL { ?parent ex:satellit ?object . }
+        FILTER (4/3 * #{Math::PI} * ?radius * ?radius * ?radius >= #{3*10**10})
+      }
+"
+    sparql = SPARQL.parse(str)
+    solutions = QUERYABLE.query(sparql)
+  end
+
+
+  # Objekte mit einem Satelliten für den ein englischsprachiger Name gegeben ist, die außerdem Satellit eines Objektes von über 3000 (km) Durchmesser sind.
+  def self.aufgabe1c
+    str = "
+      PREFIX ex: <http://example.org/>
+      SELECT DISTINCT ?object ?name WHERE
+      {
+        {
+          ?object ex:name ?name .
+          ?parent ex:radius ?radius .
+        }
+        FILTER (
+          ?radius >= 3000.0 &&
+          lang(?name) = 'en'
+        )
+      }
+"
+    sparql = SPARQL.parse(str)
+    solutions = QUERYABLE.query(sparql)
+  end
+
+  # Objekte mit zwei oder mehr Satelliten
+  def self.aufgabe1d
+    str = "
+      PREFIX ex: <http://example.org/>
+      SELECT ?object (COUNT(?satellit) AS ?sat_count) WHERE
+      {
+        ?object ex:satellit ?satellit .
+      }
+      GROUP BY ?object
+      HAVING(COUNT(?satellit) >= 2)
+"
+    sparql = SPARQL.parse(str)
+    solutions = QUERYABLE.query(sparql)
   end
 
 end
---
+++ spec/blatt06_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+require 'blatt06'
+
+describe 'Blatt06' do
+  describe 'Aufgabe 1'
+  it 'should return the correct satellites' do
+    expected_satellites = %w(Merkur Venus Erde Mars Mond Phobos Deimos).map { |s| "http://example.org/#{s}"}
+    expect(Blatt06.aufgabe1a.map { |sol| sol.satellite.to_s}).to eql(expected_satellites)
+  end
+
+  it 'should return objects with volume over 3 x 10^10 km^3' do
+    expected_objects = %w(Sonne Merkur Venus Erde Mars).map { |s| "http://example.org/#{s}"}
+    expect(Blatt06.aufgabe1b.map { |sol| sol.object.to_s}).to eql(expected_objects)
+  end
+
+  it 'should return objects with volume over 3 x 10^10 km^3 and their planet' do
+    expected_objects = %w(Sonne).map { |s| "http://example.org/#{s}"}
+    expect(Blatt06.aufgabe1b.map { |sol| sol.parent.to_s if sol.bound?(:parent)}.compact.uniq).to eql(expected_objects)
+  end
+
+  it 'should return english named satellites with parent object over 3000 km radius' do
+    expected_objects = %w(Moon)
+    expect(Blatt06.aufgabe1c.map { |sol| sol.name.to_s }).to eql(expected_objects)
+  end
+
+  it 'should return english named satellites with parent object over 3000 km radius' do
+    expected_objects = %w(Sonne Mars).map { |s| "http://example.org/#{s}"}
+    expect(Blatt06.aufgabe1d.map { |sol| sol.object.to_s }).to eql(expected_objects)
+  end
+end
